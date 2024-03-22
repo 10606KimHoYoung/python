446d56aeedb3b284993a909113823950b8b827e8
10606KimHoYoung <162937241+10606KimHoYoung@users.noreply.github.com>
2024-03-22 14:14:40 +0900

    Add files via upload

---
+++ turtle_test1.py
@@ -0,0 +1,94 @@
+# 거북이 그래픽 모듈 사용 - 한줄 주석
+
+#import turtle # turtle 모듈 내 프로그램에서 사용
+#import turtle as t # 별명을 정해줌
+
+turtle.shape("arrow") #circle arrow turtle square
+turtle.color("green")
+turtle.begin_fill()
+
+#삼각형 120 사각형 90 오각형 72 육각형 60 팔각형 45
+
+#삼각형 그리기
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+#사각형 그리기
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+#오각형 그리기
+turtle.color("red")
+turtle.begin_fill()
+turtle.forward(100)
+turtle.right(70)
+turtle.forward(100)
+turtle.right(70)
+turtle.forward(100)
+turtle.right(70)
+turtle.forward(100)
+turtle.right(70)
+turtle.forward(100)
+turtle.right(70)
+turtle.forward(100)
+turtle.right(70)
+turtle.end_fill()
+
+#육각형 그리기
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+turtle.forward(100)
+turtle.right(60)
+
+#팔각형 그리기
+turtle.color("black")
+turtle.begin_fill()
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(100)
+turtle.right(45)
+turtle.end_fill()
+
+#작품
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
